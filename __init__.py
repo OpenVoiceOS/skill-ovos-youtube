6675d2dd98fa17c91f19ec09febd9ae212a4841a
jarbasal <jarbasai@mailfence.com>
2021-08-14 14:02:15 +0100

    .

--- __init__.py
+++ __init__.py
@@ -1,8 +1,8 @@
 from os.path import join, dirname
 
 from ovos_workshop.skills.common_play import OVOSCommonPlaybackSkill
-from ovos_workshop.frameworks.playback import CPSMatchType, CPSPlayback, \
-    CPSMatchConfidence
+from ovos_workshop.frameworks.playback import CommonPlayMediaType, CommonPlayPlaybackType, \
+    CommonPlayMatchConfidence
 from ovos_utils.parse import fuzzy_match, MatchStrategy
 
 from youtube_searcher import search_youtube
@@ -11,41 +11,41 @@ from youtube_searcher import search_youtube
 class SimpleYoutubeSkill(OVOSCommonPlaybackSkill):
     def __init__(self):
         super(SimpleYoutubeSkill, self).__init__("Simple Youtube")
-        self.supported_media = [CPSMatchType.GENERIC,
-                                CPSMatchType.MUSIC,
-                                CPSMatchType.AUDIO,
-                                CPSMatchType.PODCAST,
-                                CPSMatchType.DOCUMENTARY,
-                                CPSMatchType.VIDEO]
+        self.supported_media = [CommonPlayMediaType.GENERIC,
+                                CommonPlayMediaType.MUSIC,
+                                CommonPlayMediaType.AUDIO,
+                                CommonPlayMediaType.PODCAST,
+                                CommonPlayMediaType.DOCUMENTARY,
+                                CommonPlayMediaType.VIDEO]
         self._search_cache = {}
         self.skill_icon = join(dirname(__file__), "ui", "ytube.jpg")
         if "fallback_mode" not in self.settings:
             self.settings["fallback_mode"] = False
 
     # common play
-    def CPS_search(self, phrase, media_type=CPSMatchType.GENERIC):
+    def CPS_search(self, phrase, media_type=CommonPlayMediaType.GENERIC):
         """Analyze phrase to see if it is a play-able phrase with this skill.
 
         Arguments:
             phrase (str): User phrase uttered after "Play", e.g. "some music"
-            media_type (CPSMatchType): requested CPSMatchType to search for
+            media_type (CommonPlayMediaType): requested CPSMatchType to search for
 
         Returns:
             search_results (list): list of dictionaries with result entries
             {
-                "match_confidence": CPSMatchConfidence.HIGH,
+                "match_confidence": CommonPlayMatchConfidence.HIGH,
                 "media_type":  CPSMatchType.MUSIC,
                 "uri": "https://audioservice.or.gui.will.play.this",
-                "playback": CPSPlayback.GUI,
+                "playback": CommonPlayPlaybackType.GUI,
                 "image": "http://optional.audioservice.jpg",
                 "bg_image": "http://optional.audioservice.background.jpg"
             }
         """
         # match the request media_type
         base_score = 0
-        if media_type == CPSMatchType.MUSIC:
+        if media_type == CommonPlayMediaType.MUSIC:
             base_score += 15
-        elif media_type == CPSMatchType.VIDEO:
+        elif media_type == CommonPlayMediaType.VIDEO:
             base_score += 25
 
         explicit_request = False
@@ -57,12 +57,12 @@ class SimpleYoutubeSkill(OVOSCommonPlaybackSkill):
 
         # playback_type defines if results should be GUI / AUDIO / AUDIO + GUI
         # this could be done at individual match level instead if needed
-        if media_type == CPSMatchType.AUDIO or not self.gui.connected:
-            playback = [CPSPlayback.AUDIO]
-        elif media_type != CPSMatchType.VIDEO:
-            playback = [CPSPlayback.GUI, CPSPlayback.AUDIO]
+        if media_type == CommonPlayMediaType.AUDIO or not self.gui.connected:
+            playback = [CommonPlayPlaybackType.AUDIO]
+        elif media_type != CommonPlayMediaType.VIDEO:
+            playback = [CommonPlayPlaybackType.GUI, CommonPlayPlaybackType.AUDIO]
         else:
-            playback = [CPSPlayback.GUI]
+            playback = [CommonPlayPlaybackType.GUI]
 
         # search youtube, cache results for speed in repeat queries
         if phrase in self._search_cache:
@@ -118,18 +118,18 @@ class SimpleYoutubeSkill(OVOSCommonPlaybackSkill):
                 return False
             return self.voc_match(match["title"], "documentary")
 
-        if media_type == CPSMatchType.MUSIC:
+        if media_type == CommonPlayMediaType.MUSIC:
             # only return videos assumed to be music
             # music.voc contains things like "full album" and "music"
             # if any of these is present in the title, the video is valid
             results = [r for r in results if is_music(r)]
 
-        if media_type == CPSMatchType.PODCAST:
+        if media_type == CommonPlayMediaType.PODCAST:
             # only return videos assumed to be podcasts
             # podcast.voc contains things like "podcast"
             results = [r for r in results if is_podcast(r)]
 
-        if media_type == CPSMatchType.DOCUMENTARY:
+        if media_type == CommonPlayMediaType.DOCUMENTARY:
             # only return videos assumed to be documentaries
             # podcast.voc contains things like "documentary"
             results = [r for r in results if is_documentary(r)]
@@ -145,14 +145,14 @@ class SimpleYoutubeSkill(OVOSCommonPlaybackSkill):
                 strategy=MatchStrategy.TOKEN_SET_RATIO)
 
             # small penalty to not return 100 and allow better disambiguation
-            if media_type == CPSMatchType.GENERIC:
+            if media_type == CommonPlayMediaType.GENERIC:
                 score -= 10
             if score >= 100:
-                if media_type == CPSMatchType.AUDIO:
+                if media_type == CommonPlayMediaType.AUDIO:
                     score -= 20  # likely don't want to answer most of these
-                elif media_type != CPSMatchType.VIDEO:
+                elif media_type != CommonPlayMediaType.VIDEO:
                     score -= 10
-                elif media_type == CPSMatchType.MUSIC and not is_music(match):
+                elif media_type == CommonPlayMediaType.MUSIC and not is_music(match):
                     score -= 5
 
             # youtube gives pretty high scores in general, so we allow it
@@ -164,13 +164,13 @@ class SimpleYoutubeSkill(OVOSCommonPlaybackSkill):
             return min(100, score)
 
         matches = []
-        if CPSPlayback.GUI in playback:
+        if CommonPlayPlaybackType.GUI in playback:
             matches += [{
                 "match_confidence": calc_score(r, idx),
-                "media_type": CPSMatchType.VIDEO,
+                "media_type": CommonPlayMediaType.VIDEO,
                 "length": parse_duration(r),
                 "uri": r["url"],
-                "playback": CPSPlayback.GUI,
+                "playback": CommonPlayPlaybackType.GUI,
                 "image": r["thumbnails"][-1]["url"].split("?")[0],
                 "bg_image": r["thumbnails"][-1]["url"].split("?")[0],
                 "skill_icon": self.skill_icon,
@@ -181,13 +181,13 @@ class SimpleYoutubeSkill(OVOSCommonPlaybackSkill):
 
         # audio only results after video results
         max_score = max([r["match_confidence"] for r in matches])
-        if CPSPlayback.AUDIO in playback:
+        if CommonPlayPlaybackType.AUDIO in playback:
             matches += [{
                 "match_confidence": calc_score(r, idx) - max_score,
-                "media_type": CPSMatchType.VIDEO,
+                "media_type": CommonPlayMediaType.VIDEO,
                 "length": parse_duration(r),
                 "uri": r["url"],
-                "playback": CPSPlayback.AUDIO,
+                "playback": CommonPlayPlaybackType.AUDIO,
                 "image": r["thumbnails"][-1]["url"].split("?")[0],
                 "bg_image": r["thumbnails"][-1]["url"].split("?")[0],
                 "skill_icon": self.skill_icon,
