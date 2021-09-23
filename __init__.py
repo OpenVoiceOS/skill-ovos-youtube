262e654ad731b1188842d8c8a07c76a65ffb046b
jarbasal <jarbasai@mailfence.com>
2021-09-23 17:47:28 +0100

    ocp

--- __init__.py
+++ __init__.py
@@ -1,8 +1,9 @@
 from os.path import join, dirname
 
+from json_database import JsonStorageXDG
 from ovos_utils.parse import fuzzy_match, MatchStrategy
-from ovos_workshop.frameworks.playback import CommonPlayMediaType, \
-    CommonPlayPlaybackType
+from ovos_workshop.frameworks.playback import MediaType, \
+    PlaybackType
 from ovos_workshop.skills.common_play import OVOSCommonPlaybackSkill, \
     common_play_search
 from youtube_searcher import search_youtube
@@ -11,24 +12,28 @@ from youtube_searcher import search_youtube
 class SimpleYoutubeSkill(OVOSCommonPlaybackSkill):
     def __init__(self):
         super(SimpleYoutubeSkill, self).__init__("Simple Youtube")
-        self.supported_media = [CommonPlayMediaType.GENERIC,
-                                CommonPlayMediaType.MUSIC,
-                                CommonPlayMediaType.PODCAST,
-                                CommonPlayMediaType.DOCUMENTARY,
-                                CommonPlayMediaType.VIDEO]
-        self._search_cache = {}
+        self.supported_media = [MediaType.GENERIC,
+                                MediaType.MUSIC,
+                                MediaType.PODCAST,
+                                MediaType.DOCUMENTARY,
+                                MediaType.VIDEO]
+        self._search_cache = JsonStorageXDG("simple_youtube.search.history",
+                                            subfolder="common_play")
         self.skill_icon = join(dirname(__file__), "ui", "ytube.jpg")
         if "fallback_mode" not in self.settings:
             self.settings["fallback_mode"] = False
 
         if "audio_mode" not in self.settings:
-            # audio mode favors the audio player whenever it makes sense,
+            # audio mode favors the audio _player whenever it makes sense,
             # it will cast more video types to audio (music videos,
             # full concert, lyrics videos...)
             self.settings["audio_mode"] = False
 
+        self.settings["audio_mode"] = False
+        self.settings["fallback_mode"] = False
+
     def search_youtube(self, phrase):
-        # search youtube, cache results for speed in repeat queries
+        # media youtube, cache results for speed in repeat queries
         if phrase in self._search_cache:
             results = self._search_cache[phrase]
         else:
@@ -37,16 +42,17 @@ class SimpleYoutubeSkill(OVOSCommonPlaybackSkill):
             except Exception as e:
                 # youtube can break at any time... they also love AB testing
                 # often only some queries will break...
-                self.log.error("youtube search failed!")
+                self.log.error("youtube media failed!")
                 self.log.exception(e)
                 return []
             self._search_cache[phrase] = results
+            self._search_cache.store()
         return results
 
     # matching helpers
     @staticmethod
     def parse_duration(video):
-        # parse duration into (int) seconds
+        # extract_streams duration into (int) seconds
         # {'length': '3:49'
         if not video.get("length"):
             return 0
@@ -64,14 +70,14 @@ class SimpleYoutubeSkill(OVOSCommonPlaybackSkill):
             length = int(nums[0]) * 60 * 60 + \
                      int(nums[0]) * 60 + \
                      int(nums[0])
-        # better-common_play expects milliseconds
+        # better-_player expects milliseconds
         return length * 1000
 
     def is_music(self, match):
         if self.settings["audio_mode"]:
-            return self.voc_match(match["title"], "music") or\
-                   self.voc_match(match["title"], "music_video") or \
-                   self.voc_match(match["title"], "live") and \
+            return (self.voc_match(match["title"], "music") or
+                    self.voc_match(match["title"], "music_video") or
+                    self.voc_match(match["title"], "live")) and \
                    not self.voc_match(match["title"], "video_filter")
         return self.voc_match(match["title"], "music") and \
                not self.voc_match(match["title"], "video_filter")
@@ -93,15 +99,15 @@ class SimpleYoutubeSkill(OVOSCommonPlaybackSkill):
     # common play
     @common_play_search()
     def search_youtube_music(self, phrase,
-                             media_type=CommonPlayMediaType.GENERIC):
-        # search only for media we are 100% sure is music
-        if media_type not in [CommonPlayMediaType.GENERIC,
-                              CommonPlayMediaType.MUSIC]:
+                             media_type=MediaType.GENERIC):
+        # media only for media we are 100% sure is music
+        if media_type not in [MediaType.GENERIC,
+                              MediaType.MUSIC]:
             return []
 
         # match the request media_type
         base_score = 0
-        if media_type == CommonPlayMediaType.MUSIC:
+        if media_type == MediaType.MUSIC:
             base_score += 20
 
         if self.voc_match(phrase, "youtube"):
@@ -127,17 +133,21 @@ class SimpleYoutubeSkill(OVOSCommonPlaybackSkill):
                 strategy=MatchStrategy.TOKEN_SET_RATIO)
 
             # small penalty to not return 100 and allow better disambiguation
-            if media_type == CommonPlayMediaType.GENERIC:
+            if media_type == MediaType.GENERIC:
                 score -= 10
+
+            # the title says its official!
+            if self.voc_match(match["title"], "official"):
+                score += 5
 
             return min(100, score)
 
         matches = [{
             "match_confidence": calc_score(r, idx),
-            "media_type": CommonPlayMediaType.MUSIC,
+            "media_type": MediaType.MUSIC,
             "length": self.parse_duration(r),
             "uri": "youtube//" + r["url"],
-            "playback": CommonPlayPlaybackType.AUDIO,
+            "playback": PlaybackType.AUDIO,
             "image": r["thumbnails"][-1]["url"].split("?")[0],
             "bg_image": r["thumbnails"][-1]["url"].split("?")[0],
             "skill_icon": self.skill_icon,
@@ -150,12 +160,12 @@ class SimpleYoutubeSkill(OVOSCommonPlaybackSkill):
 
     @common_play_search()
     def search_youtube_videos(self, phrase,
-                              media_type=CommonPlayMediaType.GENERIC):
+                              media_type=MediaType.GENERIC):
         # match the request media_type
         base_score = 0
-        if media_type == CommonPlayMediaType.MUSIC:
+        if media_type == MediaType.MUSIC:
             base_score += 15
-        elif media_type == CommonPlayMediaType.VIDEO:
+        elif media_type == MediaType.VIDEO:
             base_score += 25
 
         explicit_request = False
@@ -166,25 +176,25 @@ class SimpleYoutubeSkill(OVOSCommonPlaybackSkill):
             explicit_request = True
 
         # video vs audio playback
-        pb = CommonPlayPlaybackType.VIDEO
+        pb = PlaybackType.VIDEO
 
         results = self.search_youtube(phrase)
 
         # primitive results filtering
-        # music handled by the other search method
+        # music handled by the other media method
         results = [r for r in results if not self.is_music(r)]
 
         if self.settings["audio_mode"]:
             results = [r for r in results
                        if not self.voc_match(r["title"], "video_filter")]
-            pb = CommonPlayPlaybackType.AUDIO
+            pb = PlaybackType.AUDIO
 
-        if media_type == CommonPlayMediaType.PODCAST:
+        if media_type == MediaType.PODCAST:
             # only return videos assumed to be podcasts
             # podcast.voc contains things like "podcast"
             results = [r for r in results if self.is_podcast(r)]
 
-        if media_type == CommonPlayMediaType.DOCUMENTARY:
+        if media_type == MediaType.DOCUMENTARY:
             # only return videos assumed to be documentaries
             # podcast.voc contains things like "documentary"
             results = [r for r in results if self.is_documentary(r)]
@@ -200,14 +210,19 @@ class SimpleYoutubeSkill(OVOSCommonPlaybackSkill):
                 strategy=MatchStrategy.TOKEN_SET_RATIO)
 
             # small penalty to not return 100 and allow better disambiguation
-            if media_type == CommonPlayMediaType.GENERIC:
+            if media_type == MediaType.GENERIC:
                 score -= 10
+
+            # the title says its official!
+            if self.voc_match(match["title"], "official"):
+                score += 5
+
             if score >= 100:
-                if media_type == CommonPlayMediaType.AUDIO:
+                if media_type == MediaType.AUDIO:
                     score -= 20  # likely don't want to answer most of these
-                elif media_type != CommonPlayMediaType.VIDEO:
+                elif media_type != MediaType.VIDEO:
                     score -= 10
-                elif media_type == CommonPlayMediaType.MUSIC and not \
+                elif media_type == MediaType.MUSIC and not \
                         self.is_music(match):
                     score -= 5
 
@@ -221,7 +236,7 @@ class SimpleYoutubeSkill(OVOSCommonPlaybackSkill):
 
         matches = [{
             "match_confidence": calc_score(r, idx),
-            "media_type": CommonPlayMediaType.VIDEO,
+            "media_type": MediaType.VIDEO,
             "length": self.parse_duration(r),
             "uri": "youtube//" + r["url"],
             "playback": pb,
