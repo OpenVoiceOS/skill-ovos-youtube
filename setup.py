606e3962707b5367021a7e522ae8a42fd64982f2
jarbasai <jarbasai@mailfence.com>
2022-10-11 15:40:39 +0100

    setup.py template

--- setup.py
+++ setup.py
@@ -1,25 +1,69 @@
 #!/usr/bin/env python3
 from setuptools import setup
 
+
+URL = "https://github.com/OpenVoiceOS/skill-ovos-youtube"
+SKILL_CLAZZ = "SimpleYoutubeSkill"  # needs to match __init__.py class name
+PYPI_NAME = "skill-simple-youtube"  # pip install PYPI_NAME
+
+
+# below derived from github url to ensure standard skill_id
+SKILL_NAME, SKILL_AUTHOR = URL.split(".com/")[-1].split("/")
+SKILL_PKG = SKILL_NAME.lower().replace('-', '_')
+PLUGIN_ENTRY_POINT = f'{SKILL_NAME.lower()}.{SKILL_AUTHOR.lower()}={SKILL_PKG}:{SKILL_CLAZZ}'
 # skill_id=package_name:SkillClass
-PLUGIN_ENTRY_POINT = 'skill-simple-youtube.jarbasai=skill_simple_youtube:SimpleYoutubeSkill'
+
+
+def get_requirements(requirements_filename: str):
+    requirements_file = path.join(path.abspath(path.dirname(__file__)),
+                                  requirements_filename)
+    with open(requirements_file, 'r', encoding='utf-8') as r:
+        requirements = r.readlines()
+    requirements = [r.strip() for r in requirements if r.strip()
+                    and not r.strip().startswith("#")]
+    return requirements
+
+
+def find_resource_files():
+    resource_base_dirs = ("locale", "ui", "vocab", "dialog", "regex", "skill")
+    base_dir = path.dirname(__file__)
+    package_data = ["*.json"]
+    for res in resource_base_dirs:
+        if path.isdir(path.join(base_dir, res)):
+            for (directory, _, files) in walk(path.join(base_dir, res)):
+                if files:
+                    package_data.append(
+                        path.join(directory.replace(base_dir, "").lstrip('/'),
+                                  '*'))
+    return package_data
+
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+with open("./version.py", "r", encoding="utf-8") as v:
+    for line in v.readlines():
+        if line.startswith("__version__"):
+            if '"' in line:
+                version = line.split('"')[1]
+            else:
+                version = line.split("'")[1]
+
 
 setup(
-    # this is the package name that goes on pip
-    name='skill-simple-youtube',
-    version='0.0.1',
+    name=PYPI_NAME,
+    version=version,
     description='ovos common play youtube skill plugin',
-    url='https://github.com/JarbasSkills/skill-simple-youtube',
+    long_description=long_description,
+    url=URL,
     author='JarbasAi',
     author_email='jarbasai@mailfence.com',
     license='Apache-2.0',
-    package_dir={"skill_simple_youtube": ""},
-    package_data={'skill_simple_youtube': ['locale/*', 'ui/*']},
-    packages=['skill_simple_youtube'],
+    package_dir={SKILL_PKG: ""},
+    package_data={SKILL_PKG: find_resource_files()},
+    packages=[SKILL_PKG],
     include_package_data=True,
-    install_requires=["ovos-plugin-manager>=0.0.1a3",
-                      "tutubo",
-                      "ovos_workshop~=0.0.5a1"],
+    install_requires=get_requirements("requirements.txt"),
     keywords='ovos skill plugin',
     entry_points={'ovos.plugin.skill': PLUGIN_ENTRY_POINT}
 )
